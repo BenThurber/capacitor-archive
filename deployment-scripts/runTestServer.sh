521d82b1e99a4db94d712b988628d0133269a098
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-11-12 14:11:21 +1300

    Added all deployment scripts from seng302.  Added service files that can be copied to /etc/systemd/system/ of the VM durring gitlab-runner CD.

--- deployment-scripts/runTestServer.sh
+++ deployment-scripts/runTestServer.sh
@@ -1,3 +1,3 @@
-fuser -k 9499/tcp || true
+fuser -k 35503/tcp || true
 source ./test-server/envVars.txt
-java -Dspring.profiles.active=dev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=9499
+java -Dspring.profiles.active=localDev -jar test-server/libs/server-0.0.1-SNAPSHOT.jar --server.port=35503
