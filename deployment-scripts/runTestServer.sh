a900744ba3b40e56fa7e182d567e72dd28e01920
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-11-16 19:31:54 +1300

    Changed spring profile from localDev to dev

--- deployment-scripts/runTestServer.sh
+++ deployment-scripts/runTestServer.sh
@@ -2,4 +2,4 @@
 
 fuser -k 35503/tcp || true
 pwd
-java -Dspring.profiles.active=localDev -jar test-server/libs/demo-0.0.1-SNAPSHOT.jar --server.port=35503
+java -jar test-server/libs/demo-0.0.1-SNAPSHOT.jar --server.port=35503
