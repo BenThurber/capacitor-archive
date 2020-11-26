69ddbd1d1852d757c66793c3699c6537ff50ea00
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-11-26 02:03:18 +0000

    server is now served by Tomcat in a seperate docker container.  This is much better than using nohup.  Added the war pluggin to gradle and configured .gitlab-ci.yml to build spring application as a war file, which is wat Tomcat expects.

--- deployment-scripts/runTestClient.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-fuser -k 35504/tcp || true
-http-server ./test-client/dist/client/ -p 35504
--- deployment-scripts/runTestServer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-fuser -k 35503/tcp || true
-pwd
-java -jar -Dspring.profiles.active=dev test-server/libs/demo-0.0.1-SNAPSHOT.jar --server.port=35503
