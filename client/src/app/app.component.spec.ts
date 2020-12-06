90f4a93e50fa44bae79a1bc67af5c75131c89a50
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-06 23:14:37 +0000

    Created Navigation Bar

--- client/src/app/app.component.spec.ts
+++ client/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {NavbarComponent} from './navbar/navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +10,8 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        NavbarComponent
       ],
     }).compileComponents();
   });
@@ -26,10 +28,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('The Capacitor Archive');
   });
 
-  it('should render title', () => {
+  it('should render header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to the Capacitor Archive');
+    expect(compiled.querySelector('app-navbar').existence);
   });
 });
