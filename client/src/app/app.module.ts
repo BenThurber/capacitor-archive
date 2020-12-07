d343c3449c97725911b679ecac151a3a4c3d3e55
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-07 14:00:29 +1300

    Refactored navbar to nav-bar

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './nav-bar/navbar.component';
 
 @NgModule({
   declarations: [
