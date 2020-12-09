58717df396ead59ccf57c706ed1cabe0945066d8
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-09 13:13:25 +1300

    Started on the about page.  Created two photos to use.  Removed Todo list.  Changed background colour, and improved contact page.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactComponent} from './views/contact/contact.component';
 import { NotImplementedComponent} from './views/not-implemented/not-implemented.component';
+import {AboutComponent} from './views/about/about.component';
 
 const routes: Routes = [
   {path: 'contact', component: ContactComponent},
   {path: 'not-implemented', component: NotImplementedComponent},
+  {path: 'about', component: AboutComponent},
 ];
 
 @NgModule({
