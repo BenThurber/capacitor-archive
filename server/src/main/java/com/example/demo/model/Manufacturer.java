81486e15f12375a82c6822bcd56215f98cf8d3c0
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-10 10:48:20 +0000

    Made end point /manufacturer/create that creates a new manufacturer. Created Controller and Repository and ManufacturerCreateRequest class. Created a test class ManufacturerController and (retroactively) UserController.

--- server/src/main/java/com/example/demo/model/Manufacturer.java
+++ server/src/main/java/com/example/demo/model/Manufacturer.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.example.demo.payload.request.ManufacturerCreateRequest;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
@@ -15,20 +16,62 @@ public class Manufacturer {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "company_name", length = FIELD_LEN, nullable = false)
+    @Column(name = "company_name", length = FIELD_LEN, nullable = false, unique = true)
     private String companyName;
 
-    @Column(name = "open-year", columnDefinition = "SMALLINT")
+    @Column(name = "open_year", columnDefinition = "SMALLINT")
     @Type(type = "org.hibernate.type.ShortType")
     private Short openYear;
 
-    @Column(name = "close-year", columnDefinition = "SMALLINT")
+    @Column(name = "close_year", columnDefinition = "SMALLINT")
     @Type(type = "org.hibernate.type.ShortType")
     private Short closeYear;
 
-    @Column(name = "bio", length = SUMMARY_LEN)
-    private String bio;
+    @Column(name = "summary", length = SUMMARY_LEN)
+    private String summary;
 
 
+    public Manufacturer(ManufacturerCreateRequest manufacturerCreateRequest) {
+        ManufacturerCreateRequest r = manufacturerCreateRequest;
 
+        setCompanyName(r.getCompanyName());
+        setOpenYear(r.getOpenYear());
+        setCloseYear(r.getCloseYear());
+        setBio(r.getBio());
+    }
+
+    public Manufacturer() { }
+
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public Short getOpenYear() {
+        return openYear;
+    }
+
+    public void setOpenYear(Short openYear) {
+        this.openYear = openYear;
+    }
+
+    public Short getCloseYear() {
+        return closeYear;
+    }
+
+    public void setCloseYear(Short closeYear) {
+        this.closeYear = closeYear;
+    }
+
+    public String getBio() {
+        return summary;
+    }
+
+    public void setBio(String bio) {
+        this.summary = bio;
+    }
 }
