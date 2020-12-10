061819cbfe373e1c34c8d567384e3f138eabdf7d
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-10 17:48:57 +1300

    Added form validation for the case when the year the company closed is before the year the company opened.

--- client/src/app/components/manufacturer-form/manufacturer-form.component.ts
+++ client/src/app/components/manufacturer-form/manufacturer-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 
 @Component({
@@ -18,22 +18,41 @@ export class ManufacturerFormComponent implements OnInit {
       openYear: ['', [Validators.pattern(integerPattern), Validators.min(1000), Validators.max(new Date().getFullYear())]],
       closeYear: ['', [Validators.pattern(integerPattern), Validators.min(1000), Validators.max(new Date().getFullYear())]],
       summary: ['', []],
-    });
+    }, { validator: checkIfCloseYearAfterOpenYear });
   }
+
 
   ngOnInit(): void {
   }
 
   onSubmit(manufacturerData): void {
-    // Process checkout data here
-    this.manufacturerForm.reset();
+    // this.manufacturerForm.reset();
 
     console.warn('Your form has been submitted', manufacturerData);
   }
 
-  public get formControls(): any {
+  get formFields(): any {
     return this.manufacturerForm.controls;
   }
 
+  get closeYearAfterOpenYearError(): any {
+    return this.manufacturerForm.errors && this.manufacturerForm.errors.closeYearAfterOpenYear;
+  }
 
+}
+
+
+function checkIfCloseYearAfterOpenYear(c: AbstractControl): any {
+  // Safety Check
+  const openDate: number = parseInt(c.value.openYear, 10);
+  const closedDate: number  = parseInt(c.value.closeYear, 10);
+
+  if (!openDate || !closedDate) { return null; }
+
+  return (openDate <= closedDate) ? null : { closeYearAfterOpenYear: true };
+  // carry out the actual date checks here for is-endDate-after-startDate
+  // if valid, return null,
+  // if invalid, return an error object (any arbitrary name), like, return { invalidEndDate: true }
+  // make sure it always returns a 'null' for valid or non-relevant cases, and a 'non-null' object for when an error should be raised on
+  // the formGroup
 }
