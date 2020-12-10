b40275156050cfe197ecb2e24381ca1bce1457e0
Benjamin Thurber <bgt26@uclive.ac.nz>
2020-12-11 12:34:30 +1300

    Added an end point to get a menufacturer by name case insensitive

--- server/src/main/java/com/example/demo/controller/ManufacturerController.java
+++ server/src/main/java/com/example/demo/controller/ManufacturerController.java
@@ -4,8 +4,10 @@ import com.example.demo.model.Manufacturer;
 import com.example.demo.payload.request.ManufacturerCreateRequest;
 import com.example.demo.payload.response.ManufacturerResponse;
 import com.example.demo.repository.ManufacturerRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -33,11 +35,36 @@ public class ManufacturerController {
 
     /**
      * Get a manufacturer by id
-     * @param manufacturerId id of the manufacturer to get
+     * @param id id of the manufacturer to get
      */
-    @GetMapping("id/{manufacturerId}")
-    public ManufacturerResponse getManufacturerById(@PathVariable Long manufacturerId, HttpServletResponse response) {
-        Manufacturer manufacturer = manufacturerRepository.findById(manufacturerId);
+    @GetMapping("id/{id}")
+    public ManufacturerResponse getManufacturerById(@PathVariable Long id, HttpServletResponse response) {
+        Manufacturer manufacturer = manufacturerRepository.findById(id);
+
+        if (manufacturer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Invalid manufacturer id, manufacturer not found");
+        }
+
+        response.setStatus(HttpServletResponse.SC_OK);
+        return new ManufacturerResponse(manufacturer);
+    }
+
+
+    /**
+     * Get a manufacturer by companyName case insensitive.  Because company_name is unique in the database, there is
+     * only ever one Manufacturer returned.
+     * @param companyName name of the manufacturer to get
+     * @return a Manufacturer with matching companyName
+     */
+    @GetMapping("name/{companyName}")
+    public ManufacturerResponse getManufacturerByNameIgnoreCase(@PathVariable String companyName, HttpServletResponse response) {
+        Manufacturer manufacturer = manufacturerRepository.findByCompanyNameIgnoreCase(companyName);
+
+        if (manufacturer == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND,
+                    String.format("Manufacturer named %s was not found", companyName));
+        }
+
         response.setStatus(HttpServletResponse.SC_OK);
         return new ManufacturerResponse(manufacturer);
     }
--- server/src/main/java/com/example/demo/repository/ManufacturerRepository.java
+++ server/src/main/java/com/example/demo/repository/ManufacturerRepository.java
@@ -2,8 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ManufacturerRepository  extends JpaRepository<Manufacturer, Integer> {
 
     Manufacturer findById(Long id);
+    Manufacturer findByCompanyNameIgnoreCase(String companyName);
+
 }
