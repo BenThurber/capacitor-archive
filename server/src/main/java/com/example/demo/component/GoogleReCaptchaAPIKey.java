f62a3f13e260f642ac618ac6e8a67d9ec29e821e
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-06 15:52:26 +1300

    Added a service TextUtil that takes a string and capitalizes the first letter between each deliminator.

--- server/src/main/java/com/example/demo/component/GoogleReCaptchaAPIKey.java
+++ server/src/main/java/com/example/demo/component/GoogleReCaptchaAPIKey.java
@@ -1,4 +1,4 @@
-package com.example.demo.service;
+package com.example.demo.component;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
---
+++ server/src/main/java/com/example/demo/service/TextUtil.java
@@ -0,0 +1,38 @@
+package com.example.demo.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service("textUtilService")
+public class TextUtil {
+
+
+    static String title(String str) {
+        return title(str, new Character[]{' '});
+    }
+
+    /**
+     * Takes a string and capitalizes the first letter between each deliminator
+     * @param str the string to make a title
+     * @param deliminators An array of characters.  This could be [' ', '-'] that would capitalize words separated by
+     *                     spaces and dashes.
+     * @return capitalized string
+     */
+    static String title(String str, Character[] deliminators) {
+        Set<Character> deliminatorSet = new HashSet<>(Arrays.asList(deliminators));
+        char[] chars = str.toLowerCase().toCharArray();
+        boolean previousCharIsLetter = false;
+        for (int i = 0; i < chars.length; i++) {
+            if (!previousCharIsLetter && Character.isLetter(chars[i])) {
+                chars[i] = Character.toUpperCase(chars[i]);
+                previousCharIsLetter = true;
+            } else if (deliminatorSet.contains(chars[i])) {
+                previousCharIsLetter = false;
+            }
+        }
+        return String.valueOf(chars);
+    }
+}
