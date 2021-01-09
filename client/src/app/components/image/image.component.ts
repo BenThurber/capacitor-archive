5465f6f0f0fb6e9da9014d4c83f855f8c0c6f708
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-09 23:42:45 +1300

    Tried to make a src attribute by checking for the existance of files in the file system, but Angular >6 doesn't support fs.

--- client/src/app/components/image/image.component.ts
+++ client/src/app/components/image/image.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+const fs = require('fs');
 
 @Component({
   selector: 'app-image',
@@ -7,16 +8,25 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ImageComponent implements OnInit {
 
+  @Input() src: string;
   @Input('webp-src') webpSrc: string;
   @Input('fallback-src') fallbackSrc: string;
+  @Input('webp-srcset') webSrcSet: string;
+  @Input('fallback-srcset') fallbackSrcSet: string;
+  @Input() sizes: string;
+  @Input() width: string;
+  @Input() height: string;
+  @Input('usemap') useMap: string;
+  @Input('ismap') isMap: string;
+  @Input() alt: string;
 
-  webpIsSupported = false;
+  webpIsSupported: boolean = null;
 
   /**
    * checks if the browser supports webp images
    * @return boolean true if supported
    */
-  static hasWebpSupport(): boolean {
+  static supportsWebp(): boolean {
     let supported;
     const loadsWebpOnCanvas = document.createElement('canvas').toDataURL('image/webp').indexOf('data:image/webp') === 0;
     const browser = ImageComponent.getBrowser();
@@ -43,19 +53,18 @@ export class ImageComponent implements OnInit {
    * @return BrowserData the browser name and version number as a string and a number.
    */
   static getBrowser(): BrowserData {
-    let browserData: BrowserData;
     const ua = navigator.userAgent;
     let tem;
     let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
 
     if (/trident/i.test(M[1])){
       tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
-      browserData = {name: 'IE', version: (tem[1] || ''), versionNumber: parseFloat((tem[1] || ''))};
+      return {name: 'IE', version: (tem[1] || ''), versionNumber: parseFloat((tem[1] || ''))};
     }
     if (M[1] === 'Chrome'){
       tem = ua.match(/\bOPR|Edge\/(\d+)/);
       if (tem != null)   {
-        browserData = {name: 'Opera', version: tem[1], versionNumber: parseFloat(tem[1])};
+        return {name: 'Opera', version: tem[1], versionNumber: parseFloat(tem[1])};
       }
     }
     M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
@@ -64,20 +73,45 @@ export class ImageComponent implements OnInit {
     if ((tem != null)) {
       M.splice(1, 1, tem[1]);
     }
-    browserData = {
+
+    return {
       name: M[0],
       version: M[1],
       versionNumber: parseFloat(M[1]),
     };
-
-    return browserData;
   }
 
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-    this.webpIsSupported = ImageComponent.hasWebpSupport();
+    const baseSrc = this.src && this.src.replace(/\.[^/.]+$/, '');
+
+    if (baseSrc) {
+
+      if (!this.webpSrc && fs.existsSync(baseSrc + '.webp')) {
+        this.webpSrc = baseSrc + '.webp';
+      }
+
+      if (!this.fallbackSrc) {
+        for (const extension of ['png', 'jpg', 'jpeg', 'gif', 'jfif']) {
+          if (fs.existsSync(baseSrc + '.' + extension)) {
+            this.fallbackSrc = baseSrc + '.' + extension;
+            break;
+          }
+        }
+      }
+
+    }
+
+    if (!this.webpSrc) {
+      this.webpSrc = this.fallbackSrc;
+    }
+
+    this.webpIsSupported = ImageComponent.supportsWebp();
+
+    console.log(this.webpSrc);
+    console.log(this.fallbackSrc);
   }
 
 }
