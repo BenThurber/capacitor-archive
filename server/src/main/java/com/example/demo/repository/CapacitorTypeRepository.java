65e0980a88bf3f8652125b3348401a653a0faf83
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-10 17:05:55 +1300

    Changed Database interfaces to use Long instead of Integer.

--- server/src/main/java/com/example/demo/repository/CapacitorTypeRepository.java
+++ server/src/main/java/com/example/demo/repository/CapacitorTypeRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.CapacitorType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CapacitorTypeRepository extends JpaRepository<CapacitorType, Integer> {
+public interface CapacitorTypeRepository extends JpaRepository<CapacitorType, Long> {
 }
--- server/src/main/java/com/example/demo/repository/CapacitorUnitRepository.java
+++ server/src/main/java/com/example/demo/repository/CapacitorUnitRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import com.example.demo.model.CapacitorUnit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CapacitorUnitRepository extends JpaRepository<CapacitorUnit, Integer> {
+public interface CapacitorUnitRepository extends JpaRepository<CapacitorUnit, Long> {
 }
