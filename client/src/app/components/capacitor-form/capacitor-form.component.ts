c60a55aaa7a74a7a6896dc21263b35bcc3f72018
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-13 12:53:32 +1300

    Populated dropdown.  Selecting "+ Add Manufacturer" navigates to creat manufacturer page.

--- client/src/app/components/capacitor-form/capacitor-form.component.ts
+++ client/src/app/components/capacitor-form/capacitor-form.component.ts
@@ -1,15 +1,56 @@
 import { Component, OnInit } from '@angular/core';
+import {Subscription} from 'rxjs';
+import {caseInsensitiveCompare} from '../../utilities/text-utils';
+import {RestService} from '../../services/rest/rest.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-capacitor-form',
   templateUrl: './capacitor-form.component.html',
-  styleUrls: ['./capacitor-form.component.css']
+  styleUrls: ['./capacitor-form.component.css', '../../styles/animations.css']
 })
 export class CapacitorFormComponent implements OnInit {
 
-  constructor() { }
+  // Manufacturer Section
+  readonly newManufacturerOption = '+ Add Manufacturer';
+  isNavigatingToCreateManufacturer = false;
+  manufacturers$: Array<string>;
+  selectedManufacturer: {index: number, value: string};
+
+
+  constructor(private restService: RestService, private router: Router) { }
 
   ngOnInit(): void {
+    this.getManufacturerList();
+  }
+
+  getManufacturerList(): Subscription {
+    return this.restService.getAllCompanyNames().subscribe({
+      next: manufacturers => {
+        manufacturers.sort(caseInsensitiveCompare);
+        this.manufacturers$ = manufacturers;
+      },
+
+      error: () => console.error('Couldn\'t get company names')
+    });
+  }
+
+  manufacturerDropdownChanged(event): void {
+
+    if (event.target.value === this.newManufacturerOption) {
+
+      this.isNavigatingToCreateManufacturer = true;
+
+      setTimeout(() => {
+        this.router.navigate(['manufacturer', 'create']).catch(
+          () => this.isNavigatingToCreateManufacturer = false
+        );
+
+      }, 800);
+
+      this.selectedManufacturer.index = Number.isInteger(event.target.value) ? event.target.value : null;
+    }
+
   }
 
 }
