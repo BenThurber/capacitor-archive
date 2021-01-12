f55cb75da9b736a2ae1cc337b7134783d4a500fb
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-13 08:23:51 +1300

    Fixed case-insensitive issue in MySQL query.

--- server/src/main/java/com/example/demo/repository/CapacitorUnitRepository.java
+++ server/src/main/java/com/example/demo/repository/CapacitorUnitRepository.java
@@ -9,11 +9,21 @@ public interface CapacitorUnitRepository extends JpaRepository<CapacitorUnit, Lo
 
     CapacitorUnit findByCapacitanceAndVoltageAndIdentifier(Long capacitance, Integer voltage, String identifier);
 
+    /**
+     * Gets a weak entity CapacitorUnit using its identifying entities typeName and companyName.
+     *
+     * NOTE: MySQL Queries are case insensitive by default.  This is why in the query here we can compare
+     * :typeName = ct.type_name without having to use LOWER() or UPPER().  They keyword BINARY is used to make a case
+     * sensitive query as used for value.
+     * @param companyName Manufacturer owning the CapacitorType of the CapacitorUnit
+     * @param typeName The CapacitorType owning the CapacitorUnit
+     * @param value The value of the CapacitorUnit (Unique Key)
+     * @return A unique CapacitorUnit
+     */
     @Query(value =
             "SELECT * FROM capacitor_unit cu INNER JOIN capacitor_type ct ON cu.capacitor_type_id = ct.id " +
                     "INNER JOIN manufacturer m ON ct.manufacturer_id = m.id " +
-                    "WHERE LOWER(:typeName) = ct.type_name_lower AND LOWER(:companyName) = m.company_name_lower " +
-                    "AND :value = cu.value;",
+                    "WHERE :typeName = ct.type_name AND :companyName = m.company_name AND BINARY :value = cu.value;",
             nativeQuery = true)
     CapacitorUnit findByTypeNameIgnoreCaseAndCompanyNameIgnoreCaseAndValue(@Param("companyName") String companyName,
                                                                            @Param("typeName") String typeName,
