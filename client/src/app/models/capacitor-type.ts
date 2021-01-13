5d0dd75e6297c1ef5a285ddb77e9332b4d5f2c9c
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-13 23:52:54 +0000

    Created a form group API in CapacitorForm. Populated the typeName dropdown menu.  When an item is selected, it appears in the fields for type.  If "+ Add New Type" is selected, an input box appears to add a new type.

---
+++ client/src/app/models/capacitor-type.ts
@@ -0,0 +1,12 @@
+
+export class CapacitorType {
+
+  typeName: string;
+  constructionName: string;
+  startYear: number;
+  endYear: number;
+  description: string;
+  companyName: string;
+  capacitorUnitValues: Array<string>;
+
+}
