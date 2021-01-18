2095fba1ef2f0deec883d0408d5fa95c19584247
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-01-19 10:33:08 +1300

    Started on View Capacitor page using a CSS grid.

---
+++ client/src/app/views/view-capacitor/view-capacitor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-capacitor',
+  templateUrl: './view-capacitor.component.html',
+  styleUrls: ['./view-capacitor.component.css']
+})
+export class ViewCapacitorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
