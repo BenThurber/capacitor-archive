dceea3bcfebcb26ae4a96f5420cd3a73fd80eca4
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-04-03 13:21:07 +1300

    Cleaned up and modularized code in CapacitorTypeSearchResponse constructor.

--- server/src/main/java/com/example/demo/payload/response/CapacitorTypeSearchResponse.java
+++ server/src/main/java/com/example/demo/payload/response/CapacitorTypeSearchResponse.java
@@ -8,6 +8,7 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -62,11 +63,40 @@ public class CapacitorTypeSearchResponse {
         List<CapacitorUnit> capacitorUnits = ct.getCapacitorUnits();
         if (capacitorUnits == null || capacitorUnits.size() <= 0) {
             setNumberOfUnits(0);
-            return;
         } else {
             setNumberOfUnits(capacitorUnits.size());
             setLowestCapacitance(capacitorUnits.stream().min(Comparator.comparing(CapacitorUnit::getCapacitance)).get().getCapacitance());
             setHighestCapacitance(capacitorUnits.stream().max(Comparator.comparing(CapacitorUnit::getCapacitance)).get().getCapacitance());
+        }
+
+        setThumbnailUrl(capacitorUnits);
+    }
+
+    public CapacitorTypeSearchResponse() {}
+
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(CapacitorType capacitorType) {
+        setThumbnailUrl(calculateThumbnailUrl(capacitorType.getCapacitorUnits()));
+    }
+
+    public void setThumbnailUrl(Collection<CapacitorUnit> capacitorUnits) {
+        setThumbnailUrl(calculateThumbnailUrl(capacitorUnits));
+    }
+
+    /**
+     * Get a single thumbnail URL a given A list of CapacitorUnits.  Efficiently sorts all CapacitorUnits from
+     * and chooses the CapacitorUnit in the middle to take the thumbnail from.  Takes the thumbnail from the
+     * primary Photo (Photo with the lowest order property).
+     * @param capacitorUnits a List of capacitor units to choose from.
+     * @return a url string of a thumbnail
+     */
+    private String calculateThumbnailUrl(Collection<CapacitorUnit> capacitorUnits) {
+        if (capacitorUnits == null || capacitorUnits.size() <= 0) {
+            return null;
         }
 
         List<CapacitorUnit> capacitorUnitsWithPhotos = capacitorUnits
@@ -75,19 +105,16 @@ public class CapacitorTypeSearchResponse {
                 .collect(Collectors.toList());
 
         if (capacitorUnitsWithPhotos.size() <= 0) {
-            return;
+            return null;
         }
 
-        // Get the capacitor unit "In the middle" of all the units
+        // Get the median capacitor unit "In the middle" of all the sorted units
         CapacitorUnit medianCapacitorUnit = QuickSelect.select(capacitorUnitsWithPhotos.toArray(new CapacitorUnit[0]), capacitorUnitsWithPhotos.size() / 2);
         if (medianCapacitorUnit == null || medianCapacitorUnit.getPrimaryThumbnail() == null) {
-            return;
+            return null;
         }
-        setThumbnailUrl(medianCapacitorUnit.getPrimaryThumbnail().getUrl());
+        return medianCapacitorUnit.getPrimaryThumbnail().getUrl();
     }
-
-    public CapacitorTypeSearchResponse() {}
-
 
 
 }
