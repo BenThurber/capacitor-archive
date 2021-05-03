76634bfdf1c5726f4a01f5e9b15dcfb6592ada84
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-05-03 02:01:40 +0000

    Removed volume in docker-compose and removed unused lined in Dockerfile

--- dockerfiles/custom-ddclient-container/entrypoint.sh
+++ dockerfiles/custom-ddclient-container/entrypoint.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 echo "Hello World"
-#find / -name "ddclient.conf"
-#envsubst < /config/ddclient.conf > ddclient.conf.swp
-#mv ddclient.conf.swp /config/ddclient.conf
+
+envsubst < /defaults/ddclient.conf > /defaults/ddclient.conf.swp
+mv /defaults/ddclient.conf.swp /defaults/ddclient.conf
+cp /defaults/ddclient.conf /config/ddclient.conf
+cp /defaults/ddclient.conf /ddclient.conf
 
 for i in $(find -name ddclient.conf);
 do
 	echo "File:"
 	echo $i
 	grep "^password" $i
-	envsubst < $i > ddclient.conf.swp
-	mv ddclient.conf.swp $i
-	grep "^password" $i
+	#envsubst < $i > ddclient.conf.swp
+	#mv ddclient.conf.swp $i
+	#grep "^password" $i
 done
 
 /init
