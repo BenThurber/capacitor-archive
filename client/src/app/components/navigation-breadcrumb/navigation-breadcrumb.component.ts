a08fa50a0eb30ca081479b17d002b7b108c5df21
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-05-15 15:49:26 +1200

    Added a Breadcrumb Service that updates the breadcrumb (a shared service).  ViewManufacturer updates the breadcrumb.  Changed links to grey.

--- client/src/app/components/navigation-breadcrumb/navigation-breadcrumb.component.ts
+++ client/src/app/components/navigation-breadcrumb/navigation-breadcrumb.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DynamicRouterService} from '../../services/dynamic-router/dynamic-router.service';
+import {BreadcrumbService} from '../../services/breadcrumb/breadcrumb.service';
+import {NavigationStart} from '@angular/router';
 
 @Component({
   selector: 'app-navigation-breadcrumb',
@@ -10,9 +12,20 @@ export class NavigationBreadcrumbComponent implements OnInit {
 
   @Input() links: Array<{name: string, url: Array<string>}> = [{name: 'Solarus', url: ['/about']}, {name: '0.1 uf', url: ['/contact']}];
 
-  constructor(public dynamicRouter: DynamicRouterService) { }
+  constructor(public dynamicRouter: DynamicRouterService, private breadcrumbService: BreadcrumbService) {
+
+    this.breadcrumbService.changeAnnounced$.subscribe(links => {
+      links.unshift({name: 'Home', url: ['/']});
+      this.links = links;
+    });
+  }
 
   ngOnInit(): void {
+    this.dynamicRouter.router.events
+      .subscribe((event: NavigationStart) => {
+        // Clear breadcrumb on page navigation
+        this.links = [];
+      });
   }
 
 }
--- client/src/app/views/view-manufacturer/view-manufacturer.component.ts
+++ client/src/app/views/view-manufacturer/view-manufacturer.component.ts
@@ -8,6 +8,7 @@ import {CapacitorTypeSearchResponse} from '../../models/capacitor-type-search-re
 import {Title} from '@angular/platform-browser';
 import {title} from '../../utilities/text-utils';
 import {ErrorHandlerService} from '../../services/error-handler/error-handler.service';
+import {BreadcrumbService} from '../../services/breadcrumb/breadcrumb.service';
 
 @Component({
   selector: 'app-view-manufacturer',
@@ -21,7 +22,8 @@ export class ViewManufacturerComponent implements OnInit {
   capacitorTypesSearchResponseObservable: Observable<Array<CapacitorTypeSearchResponse>>;
 
   constructor(private titleService: Title, public restService: RestService, public dynamicRouter: DynamicRouterService,
-              public activatedRoute: ActivatedRoute, private errorHandler: ErrorHandlerService) {
+              public activatedRoute: ActivatedRoute, private errorHandler: ErrorHandlerService,
+              private breadcrumbService: BreadcrumbService) {
     this.companyName = this.activatedRoute.snapshot.paramMap.get('companyName');
     this.capacitorTypesSearchResponseObservable = this.restService.getAllTypeSearchResponses(this.companyName);
   }
@@ -31,7 +33,13 @@ export class ViewManufacturerComponent implements OnInit {
 
     return this.restService.getManufacturerByName(this.companyName)
       .subscribe({
-        next: (manufacturer: Manufacturer) => this.manufacturer$ = manufacturer,
+        next: (manufacturer: Manufacturer) => {
+          this.manufacturer$ = manufacturer;
+          // Update Breadcrumb Navigator
+          this.breadcrumbService.change(
+            [{name: manufacturer.companyName, url: ['/manufacturer', 'view', manufacturer.companyName.toLowerCase()]}]
+          );
+        },
         error: err => this.errorHandler.handleGetRequestError(err, 'Error getting manufacturer')
       });
   }
