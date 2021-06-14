a77c26cdde0582e90349f926bbfe2d95e4dda543
Benjamin Thurber <bgt26@uclive.ac.nz>
2021-06-14 17:06:25 +1200

    Fixed intermediate route issue by copying implementation of onClick from RouterLinkWithHref and modifying.

--- client/src/app/directives/dynamic-router-link/dynamic-router-link.directive.ts
+++ client/src/app/directives/dynamic-router-link/dynamic-router-link.directive.ts
@@ -1,5 +1,6 @@
 import {Directive, Input, OnInit} from '@angular/core';
-import {RouterLinkWithHref} from '@angular/router';
+import {ActivatedRoute, Router, RouterLinkWithHref} from '@angular/router';
+import {LocationStrategy} from '@angular/common';
 
 @Directive({
   selector: 'a[appRouterLink],area[appRouterLink]'
@@ -8,15 +9,38 @@ export class DynamicRouterLinkDirective extends RouterLinkWithHref implements On
 
   @Input() appRouterLink: Array<string>;
 
+  constructor(private router2: Router, private route2: ActivatedRoute, private locationStrategy2: LocationStrategy) {
+    super(router2, route2, locationStrategy2);
+  }
+
   ngOnInit(): void {
     this.routerLink = this.appRouterLink;
     this.href = this.appRouterLink.join('/');
   }
 
-
-  onClick(button: number, ctrlKey: boolean, shiftKey: boolean, altKey: boolean, metaKey: boolean): boolean {
-    this.routerLink = this.appRouterLink;
-    (this as any).router.navigateByUrl('/', {skipLocationChange: true});
-    return super.onClick(button, ctrlKey, shiftKey, altKey, metaKey);
+  /**
+   * Same implementation as RouterLinkWithHref but route goes to '/' then to the destination.
+   */
+  onClick(button, ctrlKey, shiftKey, altKey, metaKey): boolean {
+    if (button !== 0 || ctrlKey || shiftKey || altKey || metaKey) {
+      return true;
+    }
+    if (typeof this.target === 'string' && this.target != '_self') {
+      return true;
+    }
+    const extras = {
+      skipLocationChange: attrBoolValue(this.skipLocationChange),
+      replaceUrl: attrBoolValue(this.replaceUrl),
+      state: this.state
+    };
+    this.router2.navigateByUrl('/', {skipLocationChange: true}).then(
+      () => this.router2.navigateByUrl(this.urlTree, extras)
+    );
+    return false;
   }
+}
+
+
+function attrBoolValue(s): any {
+  return s === '' || !!s;
 }
